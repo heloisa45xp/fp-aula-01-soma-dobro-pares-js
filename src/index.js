8cd1feb6a2cf4f542703b367ec55387aeda718f1
heloisa45xp <heloisa.carvalho@estudante.ifto.edu.br>
2026-05-02 15:40:14 -0300

    Fix formatting and comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-// Arquivo de exemplo para executar as funções definidas no math.js
-
 import * as math from './math.js';
 
-const numeros = [1, 2, 3, 4, 5, 6]; // escolha os números que desejar 
-console.log('Soma usando a abordagem funcional:', math.somaDobroNumerosParesFuncional(numeros))
-console.log('Soma usando a abordagem estrutural (procedural):', math.somaDobroNumerosParesProcedural(numeros))
+const numeros = [1, 2, 3, 4, 5, 6];
+
+console.log('Soma usando a abordagem funcioonal:', math.somaDobroNumerosParesFuncional(numeros));
+console.log('Soma usando a abordagem estrutural (procedural):', math.somaDobroNumerosParesProcedural(numeros));
