e2645ead0315f719ecf698f303798605805cfece
heloisa45xp <heloisa.carvalho@estudante.ifto.edu.br>
2026-05-02 15:39:24 -0300

    Implement functional and procedural sum functions for evens

--- src/math.js
+++ src/math.js
@@ -1,23 +1,16 @@
-/**
- * Módulo para implementar funções matemáticas.
- */
-
-/**
- * Realiza a soma do dobro do valor de cada número par em um vetor de números.
- * A função deve ser implementada utilizando a abordagem funcional com o modelo map/reduce.
- * @param {Array} numeros vetor com os números desejados
- */
-function somaDobroNumerosParesFuncional(numeros){
-    // TODO: Insira seu código funcional aqui
+export function somaDobroNumerosParesFuncional(numeros) {
+    return numeros
+        .filter(n => n % 2 === 0)
+        .map(n => n * 2)
+        .reduce((soma, n) => soma + n, 0);
 }
 
-/**
- * Realiza a soma do dobro do valor de cada número par em um vetor de números.
- * A função deve ser implementada utilizando a abordagem estruturada (procedural) convencional.
- * @param {Array} numeros vetor com os números desejados
- */
-function somaDobroNumerosParesProcedural(numeros){
-    // TODO: Insira seu código estruturado aqui   
+export function somaDobroNumerosParesProcedural(numeros) {
+    let somaTotal = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 === 0) {
+            somaTotal += numeros[i] * 2;
+        }
+    }
+    return somaTotal;
 }
-
-export { somaDobroNumerosParesFuncional, somaDobroNumerosParesProcedural };
